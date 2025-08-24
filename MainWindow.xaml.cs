00cc3bc8d0d675af35fc87c425b1f4d3eafe49d1
Yogeshwar B. <byogeshwarreddy@gmail.com>
2025-08-24 20:13:45 +0530

    Dependancy Changed- FFMPEG Package. UI Changes- File Picker Added.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +18,36 @@ namespace GifMan
         public MainWindow()
         {
             InitializeComponent();
+            var hWnd = WindowNative.GetWindowHandle(this);
+
+            var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+
+            if (appWindow != null)
+            {
+                appWindow.Resize(new Windows.Graphics.SizeInt32(1920, 1080));
+            }
+        }
+
+        private async void FilePathSelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new FileOpenPicker();
+
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            InitializeWithWindow.Initialize(picker, hWnd);
+
+            picker.FileTypeFilter.Add(".mp4");
+
+            StorageFile file = await picker.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                FilePathTextBox.Text = file.Path; 
+            }
+            else
+            {
+                FilePathTextBox.Text = "No file selected";
+            }
         }
     }
 }
